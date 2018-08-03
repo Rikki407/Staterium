cc0bd597115871e0ac28f68a89b7cde59f4d2935
Rikki407 <rishablamba407@gmail.com>
2018-08-03 23:43:00 +0530

    playing with bcrypt

--- models/User-model.js
+++ models/User-model.js
@@ -1,12 +1,66 @@
-let mongoose = require('mongoose');
-let passportLocalMongoose = require('passport-local-mongoose');
+const mongoose = require('mongoose'),
+    bcrypt = require('bcrypt');
 
-let UserSchema = new mongoose.Schema({
-    email: String,
-    username: String,
-    password: String,
-    emailPassword: String
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        trim: true
+    },
+    ethAddress: {
+        type: String,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
-UserSchema.plugin(passportLocalMongoose);
+UserSchema.statics.authenticate = (email, password, callback) => {
+    User.findOne({ email }).exec((err, user) => {
+        if (err) {
+            return callback(err);
+        } else if (!user) {
+            const error = new Error('User not found.');
+            error.status = 401;
+            return callback(error);
+        }
+        bcrypt.compare(password, user.password, (error, result) => {
+            if (result === true) {
+                return callback(null, user);
+            }
+            return callback();
+        });
+    });
+};
+UserSchema.methods.ethAddressAuthenticate = (
+    ethAddress,
+    signature,
+    nonce,
+    callback
+) => {
+    User.findOne({ ethAddress }).exec((err, user) => {
+        if (err) {
+            return callback(err);
+        } else if (!user) {
+            const error = new Error('User not found.');
+            error.status = 401;
+            return callback(error);
+        }
+    });
+};
 
-module.exports = mongoose.model('User', UserSchema);
+UserSchema.pre('save', function(next) {
+    const user = this;
+    bcrypt.hash(user.password, 10, function(err, hash) {
+        if (err) {
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    });
+});
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
