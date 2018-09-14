0d229720f36c999ff577394051f0612c90f649ef
Rikki407 <rishablamba407@gmail.com>
2018-09-14 15:53:03 +0530

    update

--- config/configKeys.js
+++ config/configKeys.js
@@ -1,14 +1,14 @@
 module.exports = {
     google: {
         clientID:
-            '70922725345-tjqj5ufi5hs3vbapg82nt95m94uii8ng.apps.googleusercontent.com',
-        clientSecret: 'sdGOmfivzMbhNQ7LkylQLUqP'
+            '',
+        clientSecret: ''
     },
     facebook: {
-        clientID: '264775504371588',
-        clientSecret: '2da0709373ace54c4ea3942dcc40dfd5'
+        clientID: '',
+        clientSecret: ''
     },
     gmail: {
-        password: 'iloveradha'
+        password: ''
     }
 };
